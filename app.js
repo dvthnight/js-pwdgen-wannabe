65aebe21734525c0b26cc0f91f1b9fed4af41253
dvthnight <davide.lanotte@protonmail.com>
2022-02-03 16:15:45 +0100

    completata parte inserimento input

---
+++ app.js
@@ -0,0 +1,17 @@
+const userName = prompt("Inserisci il tuo nome")
+console.log(userName)
+
+const userSurname = prompt("Inserisci il tuo cognome")
+console.log(userSurname)
+
+const userColor = prompt("Inserisci il tuo colore preferito")
+console.log(userColor)
+
+
+
+
+
+
+
+
+
